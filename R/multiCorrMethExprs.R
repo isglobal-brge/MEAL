9e324eb52ea8f0b5cb9618d67ba83bdde8f424a9
Carlos Ruiz-Arenas <cruiz@creal.cat>
2016-10-14 10:57:10 +0000

    Bump version Add multiCorrMethExprs to defunct
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/MEAL@122427 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/multiCorrMethExprs.R
+++ R/multiCorrMethExprs.R
@@ -1,12 +1,5 @@
-#' Computes the correlation between methylation and expression in a genomic range 
-#' 
-#' This function did not work properly and it has been deprecated. It will be made defunct in the next version. 
-#' 
 #' @export multiCorrMethExprs
-#' @return Deprecated
-#' @examples 
-#' multiCorrMethExprs()
 multiCorrMethExprs <- function(){
   
-  .Deprecated(msg = "Multivariate integration of expression and methylation was found to be not useful and it has been removed.")
+  .Defunct(msg = "Multivariate integration of expression and methylation was found to be not useful and it has been removed.")
 }
