ad4b3c76f28631a31107593879d3417d00c56f3d
Carlos Ruiz <yocra3@gmail.com>
2018-05-25 15:48:24 +0200

    Solve a bug in runBumphunter

--- R/runBumphunter.R
+++ R/runBumphunter.R
@@ -110,13 +110,18 @@ runBumphunter <- function(set, model, coefficient = 2, bumphunter_cutoff = 0.1,
         bumphunter_cutoff <- bumphunter_cutoff + 0.05
         res <-  minfi::bumphunter(object = mat, design = model, coef = coefficient,
                                          chr = annot[ , "chromosome"], pos = annot[, "start"],
-                                         cutoff = bumphunter_cutoff, B = num_permutations, 
+                                         cutoff = bumphunter_cutoff, B = 0, 
                                          nullMethod = "bootstrap", verbose = verbose, ...)$table
         if (verbose){
           message(paste("Iteration",i,"Num bumps:", nrow(res), 
                         "cutoff:", bumphunter_cutoff))
         }
       }
+      res <-  minfi::bumphunter(object = mat, design = model, coef = coefficient,
+                              chr = annot[ , "chromosome"], pos = annot[, "start"],
+                              cutoff = bumphunter_cutoff, B = num_permutations, 
+                              nullMethod = "bootstrap", verbose = verbose, ...)$table
+    
    } 
     if (length(res) == 1){
       res <- data.frame()
