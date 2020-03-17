ce531e8c938ca40ad5f063abe285b0f42a955805
Carlos Ruiz <yocra3@gmail.com>
2020-03-17 12:58:20 +0100

    Update package to remove region detection functions

--- R/runDMRcate.R
+++ R/runDMRcate.R
@@ -1,13 +1,6 @@
 #' Run DMRcate
 #' 
-#' Run DMRcate to a methylation dataset. This function contains all steps of
-#' DMRcate analysis, from model.matrix creation to running the analysis. 
-#' 
-#' @details runDMRcate is a wrapper for \code{dmrcate} function. runDMRcate  
-#' runs all the steps required prior running blockFinder from the methylation set
-#' and the formula of the model. This implementation allows running blockFinder to 
-#' other objects than \code{GenomicRatioSet}. The result can be encapsulated in a 
-#' \code{ResultSet} to take adavantege of its plotting capabilities.
+#' @details This function has been deprecated and will be defunct in the new version.
 #' @export
 #' 
 #' @param set \code{GenomicRatioSet}, \code{eSet} derived object or 
@@ -21,59 +14,5 @@
 #' @seealso \code{\link[DMRcate]{dmrcate}}, \code{\link[DMRcate]{cpg.annotate}}
 runDMRcate <- function(set, model, coefficient = 2, resultSet = FALSE, ...){
   
-  ## Create model matrix from formula
-  if (is(model, "formula")){
-    model <- createModel(set, model)
-    set <- set[, rownames(model)]
-  }
-  
-  
-  ## Get matrix
-  if (is(set, "GenomicRatioSet")){
-    mat <- set
-  } else if (is(set, "SummarizedExperiment")){
-    mat <- SummarizedExperiment::assay(set)
-  } else {
-    stop("set must be a MethylationSet, GenomicRatioSet or SummarizedExperiment.")
-  }
-  
-  annParams <- formals(DMRcate::cpg.annotate)
-  annParams <- annParams[!names(annParams) %in% c("object", "design", "what", "coef")]
-  
-  dmrParams <- formals(DMRcate::dmrcate)
-  dmrParams <- dmrParams[!names(dmrParams) %in% c("object")]
-  
-  dots <- list(...)
-
-  annParams[intersect(names(dots), names(annParams))] <- 
-    dots[intersect(names(dots), names(annParams))]
-  
-  dmrParams[intersect(names(dots), names(dmrParams))] <- 
-    dots[intersect(names(dots), names(dmrParams))]
-  
-  myannotation <- do.call(DMRcate::cpg.annotate, 
-                          c(list(object = mat, design = model, 
-                                 what = "M", coef = coefficient), annParams))
-
-  res <-   tryCatch(as(DMRcate::extractRanges(do.call(DMRcate::dmrcate,
-                        c(list(object = myannotation), dmrParams))), "data.frame"),
-                        error = function(err) {
-                          if(grepl("The FDR you specified in ", as.character(err)) | 
-                             grepl("sum(object$sig) >= 2", as.character(err)) |
-                             grepl("K >= 2", as.character(err))) {
-                            warning("No significant CpGs were found.")
-                          } else {
-                            stop(err)
-                          }
-                          return(data.frame())
-                        })
-  if (resultSet)
-  {
-    fFun <- getFeatureDataFun(set)
-    
-    res <- create_resultset("runDMRcate", lResults = 
-                              list(dmrcate = list(result = res, error = NA)),  
-                            fData = list(main = fFun(set)), lOptions = list())
-  }
-  res
+  .Deprecated()
 }
